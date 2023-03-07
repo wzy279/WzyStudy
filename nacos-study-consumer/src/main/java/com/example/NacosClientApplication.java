26a5fb1c86ee9949deb42dd71cba9b41d154e732
wzy <wzy@qq.com>
2023-03-07 10:32:06 +0800

    nacos

--- nacos-study-consumer/src/main/java/com/example/NacosClientApplication.java
+++ nacos-study-consumer/src/main/java/com/example/NacosClientApplication.java
@@ -1,9 +1,11 @@
 package com.example;
 
+import com.alibaba.cloud.nacos.loadbalancer.NacosLoadBalancerClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,14 +14,18 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableDiscoveryClient
 //@EnableFeignClients
 @EnableWebMvc
+@LoadBalancerClient(value = "nacos-provider",configuration = NacosLoadBalancerClientConfiguration.class)
 public class NacosClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(NacosClientApplication.class,args);
 
     }
     @Bean
-    @LoadBalanced
+    @LoadBalanced//开启负载均衡
+
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+
 }
